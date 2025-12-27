cea9ca34acd882e458aa093cafbdfba12c648ee4
riotaro <rugby117117@gmail.com>
2025-12-28 05:35:41 +0900

    ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Layout from './Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NoteDetail from './pages/NoteDetail';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import { Home } from './pages/Home';
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/notes/:id" element={<NoteDetail />} />
+          </Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
