55961ec76b73531bb773d90a0b895ce3da616df5
riotaro <rugby117117@gmail.com>
2026-01-02 06:03:39 +0900

    各アクションからノートの詳細ページに遷移できるように

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -5,6 +5,7 @@ import { useCurrentUserStore } from '@/modules/auth/current-user.state';
 import { noteRepository } from '@/modules/notes/note.repository';
 import { Note } from '@/modules/notes/note.entity';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface NoteListProps {
   layer?: number;
@@ -12,6 +13,7 @@ interface NoteListProps {
 }
 
 export function NoteList({ layer = 0, parentId }: NoteListProps) {
+  const navigate = useNavigate();
   const noteStore = useNoteStore();
   const notes = noteStore.getAll();
   const { currentUser } = useCurrentUserStore();
@@ -25,6 +27,7 @@ export function NoteList({ layer = 0, parentId }: NoteListProps) {
     const newNote = await noteRepository.create(currentUser!.id, { parentId });
     noteStore.set([newNote]);
     setExpanded((prev) => prev.set(parentId, true));
+    moveToDetail(newNote.id);
   };
 
   // ノートの小要素の取得
@@ -39,6 +42,11 @@ export function NoteList({ layer = 0, parentId }: NoteListProps) {
       newExpanded.set(note.id, !prev.get(note.id));
       return newExpanded;
     });
+  };
+
+  // ページを遷移する
+  const moveToDetail = (noteId: number) => {
+    navigate(`/notes/${noteId}`);
   };
 
   return (
@@ -63,6 +71,7 @@ export function NoteList({ layer = 0, parentId }: NoteListProps) {
                 note={note}
                 layer={layer}
                 expanded={expanded.get(note.id)}
+                onClick={() => moveToDetail(note.id)}
                 onExpand={(e: React.MouseEvent) => fetchChildren(e, note)}
                 onCreate={(e) => createChild(e, note.id)}
               />
