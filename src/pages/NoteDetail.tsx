adf4d11cfedb4fe8a562bdaf1859e6e7451f2671
riotaro <rugby117117@gmail.com>
2026-01-01 14:02:49 +0900

    特定のIDのノートを取得する

--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -1,6 +1,35 @@
 import { TitleInput } from '@/components/Toolbar';
+import { useCurrentUserStore } from '@/modules/auth/current-user.state';
+import { noteRepository } from '@/modules/notes/note.repository';
+import { useNoteStore } from '@/modules/notes/note.state';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const NoteDetail = () => {
+  // URLの末尾を取得するにはuseParamsが使える
+  const params = useParams();
+  //取得したparams.idは文字列になっているので整数に変換しておく
+  const id = parseInt(params.id!);
+  const [isLoading, setIsLoading] = useState(false);
+  const { currentUser } = useCurrentUserStore();
+  const noteStore = useNoteStore();
+  const note = noteStore.getOne(id);
+
+  useEffect(() => {
+    const fetchOne = async () => {
+      setIsLoading(true);
+      const note = await noteRepository.findOne(currentUser!.id, id);
+      if (note == null) return;
+      noteStore.set([note]);
+      setIsLoading(false);
+    };
+    fetchOne();
+  }, [id]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (note == null) return <div>note is not existed</div>;
+  console.log(note);
+
   return (
     <div className="pb-40 pt-20">
       <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
