f2b5da24844ef0cb90ca4a14cea073cfb6b212b9
riotaro <rugby117117@gmail.com>
2026-01-04 07:09:56 +0900

    blocknote修正

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,7 +2,8 @@
 import { useCreateBlockNote } from '@blocknote/react';
 import '@blocknote/mantine/style.css';
 import { BlockNoteView } from '@blocknote/mantine';
-import { ja } from '@blocknote/core/locales';
+// import { ja } from '@blocknote/core/locales';
+import { locales } from '@blocknote/core';
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -11,7 +12,7 @@ interface EditorProps {
 
 function Editor({ onChange, initialContent }: EditorProps) {
   const editor = useCreateBlockNote({
-    dictionary: ja,
+    dictionary: locales.ja,
     initialContent: initialContent != null ? JSON.parse(initialContent) : undefined, //初期値の設定
   });
   console.log(editor)
